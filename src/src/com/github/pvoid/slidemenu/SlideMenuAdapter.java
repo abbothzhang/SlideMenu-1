92d538d9fb91baf769cb576d7c7c23a755cdcce5
pvoid <pointervoid@gmail.com>
2013-01-14 00:31:48 +0400

    [*] comments for SlideMenuAdapter class

--- src/src/com/github/pvoid/slidemenu/SlideMenuAdapter.java
+++ src/src/com/github/pvoid/slidemenu/SlideMenuAdapter.java
@@ -2,8 +2,16 @@ package com.github.pvoid.slidemenu;
 
 import android.support.v4.app.Fragment;
 import android.widget.BaseAdapter;
-
+/**
+ * Adapter for slide menu and content frame
+ */
 public abstract class SlideMenuAdapter extends BaseAdapter
 {
+  /**
+   * Should return fragment for menu item
+   * @param position position of menu item
+   * @param id       identification of menu item
+   * @return Fragment or null. If null will be returned content won't be changed
+   */
   public abstract Fragment getFragment(int position, long id);
 }
